58a0a7b7a56d867c0fa14e3a38b93cf40e8810ac
samsiebert <sammsiebert@gmail.com>
2022-05-31 17:34:36 -0500

    add reset operation for vars at every button push, add comments for readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,20 +10,29 @@ var usableChars = "";
   
 
 var generatePassword = function() {
+  var passwordLength = "";
+  var needLower = "";
+  var needUpper = "";
+  var needNumber = "";
+  var needSpecial = "";
+  var finalPassword = "";
+  usableChars = "";
 
-  // location.reload();
+  //get required password length
+  passwordLength = parseInt(prompt("How many characters long does your password need to be? Please input a number 8-128."));
 
-  var passwordLength = parseInt(prompt("How many characters long does your password need to be? Please input a number 8-128."));
-
-  var needLower = 
+  //get password criteria
+  needLower = 
     window.confirm("Select 'OK' if your password requires at least one lowercase letter. If not, select 'Cancel'.");
-  var needUpper = 
+  needUpper = 
     window.confirm("Select 'OK' if your password requires at least one uppercase letter. If not, select 'Cancel'."); 
-  var needNumber = 
+  needNumber = 
     window.confirm("Select 'OK' if your password requires at least one numeric character. If not, select 'Cancel'.");
-  var needSpecial = 
+  needSpecial = 
     window.confirm("Select 'OK' if your password requires at least one special character. If not, select 'Cancel'.");
 
+
+  //based on entered password criteria, put elligible chracters in a string of usable characters
   if (needLower) { 
     usableChars = usableChars + lowerCase;
   }
@@ -37,6 +46,7 @@ var generatePassword = function() {
     usableChars = usableChars + specialChars;
   }
 
+//for entered password length, generate random chracter from usable characters
 for (i=0; i < passwordLength; i++) {
 
   var randomChar = usableChars[Math.floor(Math.random() * usableChars.length)];
